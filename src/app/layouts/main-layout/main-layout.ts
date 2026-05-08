d70fb0a73a5d87658a125e0aa7ae6a73bf58db65
unabia.hazeljoy123@gmai.com <jillanekate119@gmail.com>
2026-05-09 00:08:53 +0800

    update project

--- src/app/layouts/main-layout/main-layout.ts
+++ src/app/layouts/main-layout/main-layout.ts
@@ -2,12 +2,14 @@ import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
 import { AuthService, User } from '../../services/auth';
+
 import Swal from 'sweetalert2';
+import { StudentNotificationService } from '../../services/student-notif';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterModule, RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterModule, RouterLink, RouterLinkActive],
   templateUrl: './main-layout.html',
   styleUrls: ['./main-layout.scss'],
   host: { style: 'display: block; width: 100%; height: 100vh;' },
@@ -19,6 +21,8 @@ export class MainLayout implements OnInit {
   currentUser: User | null = null;
 
   private platformId = inject(PLATFORM_ID);
+  readonly studentNotifService = inject(StudentNotificationService);
+  studentNotif: any;
 
   constructor(
     public auth: AuthService,
@@ -32,6 +36,11 @@ export class MainLayout implements OnInit {
       this.isProfileMenuOpen = false;
       this.sidebarOpen = false;
     });
+
+    if (this.isStudent()) {
+      this.studentNotif = new StudentNotificationService();
+      this.studentNotif.loadNotifications();
+    }
   }
 
   loadUser(): void {
@@ -66,6 +75,9 @@ export class MainLayout implements OnInit {
     if (this.isElecom()) {
       this.router.navigate(['/app/elecom-notifications']);
     }
+    if (this.isStudent()) {
+      this.router.navigate(['/app/student-notifications']);
+    }
   }
 
   goToSettings(event: Event): void {
@@ -94,9 +106,8 @@ export class MainLayout implements OnInit {
       confirmButtonText: 'Yes, logout!',
       cancelButtonText: 'Cancel',
     }).then(async (result) => {
-      // ✅ async added
       if (result.isConfirmed) {
-        await this.auth.logout(); // ✅ await added
+        await this.auth.logout();
         Swal.fire({
           title: 'Logged out!',
           icon: 'success',
